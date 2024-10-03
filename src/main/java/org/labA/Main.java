0f13c3bb03512412d781aa47843853161e201c63
aqppaulet <jparedesma@unsa.edu.pe>
2024-10-02 23:41:47 -0500

    Initial commit

---
+++ src/main/java/org/labA/Main.java
@@ -0,0 +1,17 @@
+package org.labA;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Mensaje de "Hola Mundo"
+        System.out.println("Hola Mundo!\n");
+
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
